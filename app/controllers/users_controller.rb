684039cce99e7b4e66b06d3b86f64b96aea98aaa
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-24 15:05:28 -0700

    user edit works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,9 +27,22 @@ class UsersController < ApplicationController
    end
 
    def edit
+      current_user
+      id = params[:id]
+      @user = User.find_by_id(id)
+      render :edit
    end
 
    def update
+      user_id = params[:id]
+      @user = User.find_by_id(user_id)
+      updated_attributes = params.require(:user).permit(:username, :first_name, :last_name, :pseudonym, :password)
+
+      @user.update_attributes(updated_attributes)
+
+      @user.save
+
+      redirect_to "/users/#{user_id}"
    end
 
    def destroy
@@ -42,55 +55,3 @@ class UsersController < ApplicationController
    end
 
 end
-
-
-# def show
-#   @user = User.find(params[:id])
-#   @submissions = @user.submissions
-#   @submissions = @submissions.order("created_at DESC")
-#   @user_edits = @user.edits
-#
-#   @current_user = current_user
-# end
-#
-#
-# def create
-#
-#    @user = User.create(user_params)
-#    #find user if create was successful
-#    createdUser = User.find_by_id(@user.id)
-#    if createdUser
-#       login(@user)
-#       redirect_to "/users/#{@user.id}"
-#    else
-#       redirect_to "/users/new"
-#    end
-# end
-#
-# def edit
-#    current_user
-#    id = params[:id]
-#    @user = User.find_by_id(id)
-#    render :edit
-# end
-#
-# def update
-#    user_id = params[:id]
-#    @user = User.find_by_id(user_id)
-#    updated_attributes = params.require(:user).permit(:username, :first_name, :last_name, :pseudonym, :password)
-#
-#    @user.update_attributes(updated_attributes)
-#
-#    @user.save
-#
-#    redirect_to "/users/#{user_id}"
-# end
-#
-# def destroy
-# end
-#
-# private
-#
-# def user_params
-#    params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
-# end
