413ef51168049c84e5fd5c9a7417a52221f6191e
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-24 12:20:49 -0700

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+end
+
+
+# def new #login page
+#       @user = User.new
+#       render :new #check
+#    end
+#
+#    def create # login
+#       userParams = params.require(:user).permit(:email, :password)
+#       @user = User.confirm(userParams)
+#       if @user
+#          login(@user)
+#          redirect_to profile_path(@user.id) #check
+#       else
+#          redirect_to "/sessions/new" #check
+#       end
+#    end
+#
+#    def destroy #logout
+#       logout
+#       redirect_to root_path #check
+#    end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,90 @@
+class UsersController < ApplicationController
+
+   def new
+      @user = User.new
+      render :new
+   end
+
+   def create
+      @user = User.create(user_params)
+      #find user if create was successful
+      createdUser = User.find_by_id(@user.id)
+      if createdUser
+         login(@user)
+         redirect_to "/users/#{@user.id}"
+      else
+         redirect_to "/users/new"
+      end
+   end
+
+   def show
+   end
+
+   def edit
+   end
+
+   def update
+   end
+
+   def destroy
+   end
+
+   private
+
+   def user_params
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+   end
+   
+end
+
+
+# def show
+#   @user = User.find(params[:id])
+#   @submissions = @user.submissions
+#   @submissions = @submissions.order("created_at DESC")
+#   @user_edits = @user.edits
+#
+#   @current_user = current_user
+# end
+#
+#
+# def create
+#
+#    @user = User.create(user_params)
+#    #find user if create was successful
+#    createdUser = User.find_by_id(@user.id)
+#    if createdUser
+#       login(@user)
+#       redirect_to "/users/#{@user.id}"
+#    else
+#       redirect_to "/users/new"
+#    end
+# end
+#
+# def edit
+#    current_user
+#    id = params[:id]
+#    @user = User.find_by_id(id)
+#    render :edit
+# end
+#
+# def update
+#    user_id = params[:id]
+#    @user = User.find_by_id(user_id)
+#    updated_attributes = params.require(:user).permit(:username, :first_name, :last_name, :pseudonym, :password)
+#
+#    @user.update_attributes(updated_attributes)
+#
+#    @user.save
+#
+#    redirect_to "/users/#{user_id}"
+# end
+#
+# def destroy
+# end
+#
+# private
+#
+# def user_params
+#    params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+# end
