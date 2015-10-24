0849f69e2b3c65c01a19dabf8522f96b531b364f
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-24 16:43:13 -0700

    maps model/controller created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024013702) do
+ActiveRecord::Schema.define(version: 20151024232232) do
+
+  create_table "maps", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
