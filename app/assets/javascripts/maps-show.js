fba82a6443dd94b1e94ad68e04e9883cb2c81837
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-28 16:15:42 -0700

    versions are associated with maps and can be rendered to map show page.

---
+++ app/assets/javascripts/maps-show.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   console.log("Sanity: Maps Show JS File");
+
+   
+});
+
+
+
+
+
+// var img = document.getElementById('myImageId');
+//    img.style.visibility = 'visible';
+
+
+
+
+
+//
