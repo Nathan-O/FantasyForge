1205db09227a923f07bf7232944ea22e15363359
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-28 17:15:10 -0700

    MVPgit add .git add .git add .git add .git add .git add .git add .git add .

--- app/assets/javascripts/maps-show.js
+++ app/assets/javascripts/maps-show.js
@@ -1,18 +1,30 @@
 $(document).ready(function(){
    console.log("Sanity: Maps Show JS File");
 
-   
+   $("#Master").show();
+
+   $("#version-select").on("change", function(e){
+      renderSelectedMap(e);
+   });
 });
 
+var previousSelect;
+var currentSelect;
 
+function renderSelectedMap(event){
+   console.log("Changed");
+   previousSelect = previousSelect || null;
+   console.log(previousSelect);
+   currentSelect = $( "#version-select option:selected" ).text();
 
+   console.log(currentSelect + " was selected");
 
-
-// var img = document.getElementById('myImageId');
-//    img.style.visibility = 'visible';
-
-
-
-
-
-//
+   if (previousSelect === null){
+      $("#Master").hide();
+      $("#" + currentSelect).show();
+   } else {
+      $("#" + previousSelect).hide();
+      $("#" + currentSelect).show();
+   }
+   previousSelect = currentSelect;
+}
